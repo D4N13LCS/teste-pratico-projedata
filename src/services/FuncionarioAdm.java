c1632c800ab352720541b841ad4acf878e9cf15c
D4N13LCS <danielmazzini6@gmail.com>
2026-04-19 20:26:13 -0300

    Feat: Adição do metodo listarTodosFuncionarios

--- src/services/FuncionarioAdm.java
+++ src/services/FuncionarioAdm.java
@@ -1,7 +1,10 @@
 package services;
 import entities.Funcionario;
+import java.text.NumberFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class FuncionarioAdm{
     private final List<Funcionario> funcionarios = new ArrayList<>();
@@ -14,4 +17,18 @@ public class FuncionarioAdm{
         return this.funcionarios;
     }    
 
+    public void listarTodosFuncionarios(String datePattern){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
+
+        NumberFormat nf = NumberFormat.getNumberInstance(new Locale("pt", "BR"));
+        nf.setMinimumFractionDigits(2);
+        
+        for (Funcionario func: this.getFuncionarios()){
+                    System.out.println(func.getId()
+                    + " " + func.getNome() 
+                    + " " + func.getDataNascimento().format(formatter)
+                    + " " + nf.format(func.getSalario())
+                    + " " + func.getFuncao());
+                }
+    }
 }
