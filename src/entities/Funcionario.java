37d5f2884f526a6d08f46750fdcf3dc89b8ec406
D4N13LCS <danielmazzini6@gmail.com>
2026-04-19 20:24:16 -0300

    Feat: Adição do metodo getId e toString

--- src/entities/Funcionario.java
+++ src/entities/Funcionario.java
@@ -3,11 +3,13 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Funcionario extends Pessoa{
+    private final Integer id;
     private BigDecimal salario;
     private String funcao;
 
-    public Funcionario(String nome, LocalDate dataNascimento,BigDecimal salario, String funcao){
+    public Funcionario(Integer id, String nome, LocalDate dataNascimento,BigDecimal salario, String funcao){
         super(nome, dataNascimento);
+        this.id = id;
         this.salario = salario;
         this.funcao = funcao;
     }
@@ -28,5 +30,12 @@ public class Funcionario extends Pessoa{
         this.funcao = funcao;
     }
 
-    
+    public Integer getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString(){
+        return this.getNome();
+    }
 }
