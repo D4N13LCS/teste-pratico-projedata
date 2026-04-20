fd2620c1c2cba1060173919b8bc8192d1354a010
D4N13LCS <danielmazzini6@gmail.com>
2026-04-19 21:02:46 -0300

    Feat: Criação do metodo cadastrarFuncionarios

--- src/services/FuncionarioAdm.java
+++ src/services/FuncionarioAdm.java
@@ -1,7 +1,15 @@
 package services;
 import entities.Funcionario;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class FuncionarioAdm{
     private final List<Funcionario> funcionarios = new ArrayList<>();
@@ -14,10 +22,37 @@ public class FuncionarioAdm{
         return this.funcionarios;
     }    
 
+
     public void adicionarFuncionario(Funcionario funcionario) {
         this.funcionarios.add(funcionario);
     }
 
+    public void cadastrarFuncionarios(String dirPath, String datePattern){
+        Funcionario funcionario;
+        
+        
+        try (Scanner sc = new Scanner(new File(dirPath))) {
+            
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
+            Integer funcId = 0;
+            while (sc.hasNextLine()) {
+                String[] line = sc.nextLine().split(",");
+
+                funcionario = new Funcionario(funcId+=1,
+                    line[0], 
+                    LocalDate.parse(line[1], formatter),
+                    new BigDecimal(line[2]), 
+                    line[3]
+                    );
+                
+                this.adicionarFuncionario(funcionario);
+                
+            }
+        }catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado: " + e.getMessage());
+        }
+    
+    }
     
     public void listarTodosFuncionarios(String datePattern){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
@@ -33,4 +68,6 @@ public class FuncionarioAdm{
                     + " " + func.getFuncao());
                 }
     }
+
+   
 }
